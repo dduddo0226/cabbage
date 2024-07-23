c271eb3ac9f9dad42c3bbc3e4308defdf7240840
tktkaj <tktkaj@naver.com>
2024-07-23 17:59:54 +0900

    fix: logincontroller 매핑오류 수정

---
+++ src/main/java/com/cabbage/spring/member/MemberController.java
@@ -0,0 +1,19 @@
+package com.cabbage.spring.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/member")
+@Controller
+public class MemberController {
+		
+	@RequestMapping("/login")
+	public String login() {
+		return "login/loginpage";
+	}
+	
+	@RequestMapping("/joinMember")
+	public String joinMember() {
+		return "login/memberjoin";
+	}
+}
